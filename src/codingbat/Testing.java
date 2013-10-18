c53259b6b0f615de3a23ce83ad972c33b04d5f62
raeffu <raeffu@raeffu.ch>
2013-10-18 13:23:15 +0100

    new Tests

--- src/codingbat/Testing.java
+++ src/codingbat/Testing.java
@@ -1,18 +1,17 @@
 package codingbat;
 
-
 // You and your date are trying to get a table at a restaurant. The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is the stylishness of your date's clothes. The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes. If either of you is very stylish, 8 or more, then the result is 2 (yes). With the exception that if either of you has style of 2 or less, then the result is 0 (no). Otherwise the result is 1 (maybe).
 
 public class Testing {
 
   public static void main(String[] args) {
-
-    Testing test = new Testing();
-
-    System.out.println(">" + test.modulo(8, 10) + "<");
+    
+    double y = 0;
+    double x= 0;
+    
+    if(1+x > Math.pow(x, Math.sqrt(2))) y=y+x;
+    
   }
 
-  public int modulo(int a, int b) {
-    return a%b;
-  }
+
 }
