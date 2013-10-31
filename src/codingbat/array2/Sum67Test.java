b67d8498b0f180fa3c2b0ab2897e38b90816aa59
raeffu <raeffu@raeffu.ch>
2013-10-31 10:58:08 +0100

    initial task sum67

---
+++ src/codingbat/array2/Sum67Test.java
@@ -0,0 +1,30 @@
+package codingbat.array2;
+
+/*
+ * Return the sum of the numbers in the array, 
+ * except ignore sections of numbers starting with a 6 and extending to 
+ * the next 7 (every 6 will be followed by at least one 7). 
+ * Return 0 for no numbers. 
+ * 
+ * sum67({1, 2, 2}) = 5
+ * sum67({1, 2, 2, 6, 99, 99, 7}) = 5
+ * sum67({1, 1, 6, 7, 2}) = 4
+ * 
+ */
+
+public class Sum67Test {
+
+  public static void main(String[] args) {
+    Sum67Test test = new Sum67Test();
+
+    System.out.println(">" + test.sum67(new int[] {1, 2, 2}) + "<");
+    System.out.println(">" + test.sum67(new int[] {1, 2, 2, 6, 99, 99, 7})
+        + "<");
+    System.out.println(">" + test.sum67(new int[] {1, 1, 6, 7, 2}) + "<");
+  }
+
+  public int sum67(int[] nums) {
+    return 0;
+  }
+
+}
