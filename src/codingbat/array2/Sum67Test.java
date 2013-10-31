94b2f3964427ed770619e6ee5aa1e38a74982d73
raeffu <raeffu@raeffu.ch>
2013-10-31 11:48:38 +0100

    complete task sum67

--- src/codingbat/array2/Sum67Test.java
+++ src/codingbat/array2/Sum67Test.java
@@ -19,12 +19,30 @@ public class Sum67Test {
 
     System.out.println(">" + test.sum67(new int[] {1, 2, 2}) + "<");
     System.out.println(">" + test.sum67(new int[] {1, 2, 2, 6, 99, 99, 7})
-        + "<");
+                       + "<");
     System.out.println(">" + test.sum67(new int[] {1, 1, 6, 7, 2}) + "<");
   }
 
   public int sum67(int[] nums) {
-    return 0;
+    int sum = 0;
+    boolean ignore = false;
+    
+    for (int i = 0; i < nums.length; i++) {
+      
+      if (nums[i] == 6) {
+        ignore = true;
+      }
+      else if (i > 0 && nums[i-1] == 7) {
+        ignore = false;
+      }
+     
+      if (!ignore) {
+        sum += nums[i];
+      }
+ 
+    }
+    
+    return sum;
   }
 
 }
