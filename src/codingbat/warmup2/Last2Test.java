66c55549a79b1326b2c8e0148a6c1c8fa1fd1920
raeffu <raeffu@raeffu.ch>
2013-12-11 12:59:37 +0000

    add task last2

---
+++ src/codingbat/warmup2/Last2Test.java
@@ -0,0 +1,46 @@
+package codingbat.warmup2;
+
+/*
+ * Given a string, return the count of the number of times that
+ * a substring length 2 appears in the string and also as the
+ * last 2 chars of the string, so "hixxxhi" yields 1
+ * (we won't count the end substring).
+ *
+ * last2("hixxhi") = 1
+ * last2("xaxxaxaxx") = 1
+ * last2("axxxaaxx") = 2
+ */
+
+public class Last2Test
+{
+
+  public static void main(String[] args)
+  {
+
+    Last2Test test = new Last2Test();
+
+    System.out.println(">" + test.last2("hixxhi") + "<");
+    System.out.println(">" + test.last2("xaxxaxaxx") + "<");
+    System.out.println(">" + test.last2("axxxaaxx") + "<");
+  }
+
+  public int last2(String str)
+  {
+    int count = 0;
+
+    if (str.length() < 2)
+      return 0;
+
+    String end = str.substring(str.length() - 2);
+
+    for (int i = 0; i < str.length() - 2; i++)
+    {
+      String sub = str.substring(i, i + 2);
+      if (sub.equals(end))
+      {
+        count++;
+      }
+    }
+    return count;
+  }
+}
