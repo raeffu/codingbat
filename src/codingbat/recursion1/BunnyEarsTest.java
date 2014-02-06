25d94ff9eba130984ba5a8629a038fb55239b898
raeffu <raeffu@raeffu.ch>
2014-02-06 14:14:10 +0000

    added new recursion1 tasks

---
+++ src/codingbat/recursion1/BunnyEarsTest.java
@@ -0,0 +1,28 @@
+package codingbat.recursion1;
+
+/*
+ * We have a number of bunnies and each bunny has two big floppy ears. 
+ * We want to compute the total number of ears across all the bunnies 
+ * recursively (without loops or multiplication). 
+ * 
+ * bunnyEars(0) = 0
+ * bunnyEars(1) = 2
+ * bunnyEars(2) = 4
+ */
+
+public class BunnyEarsTest {
+  
+  public static void main(String[] args) {
+    BunnyEarsTest test = new BunnyEarsTest();
+
+    System.out.println(">" + test.bunnyEars(0) + "<");
+    System.out.println(">" + test.bunnyEars(1) + "<");
+    System.out.println(">" + test.bunnyEars(2) + "<");
+  }
+
+  public int bunnyEars(int bunnies) {
+    if (bunnies == 0) return 0;
+    
+    return 2 + bunnyEars(bunnies-1);
+  }
+}
